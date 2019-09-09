e8d2256752bb1f6a3389d2de2b20a7221aa0d2be
Eric Cheng <ericcheng986@gmail.com>
2019-09-09 01:49:26 -0400

    removed scss

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import "bootstrap/dist/css/bootstrap.css";
-import "./sass/styles.scss"
+import "./css/styles.css"
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
