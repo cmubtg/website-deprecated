68003b214608512c21c93ac8c8aa233913c35b3e
Eric Cheng <ericcheng986@gmail.com>
2019-12-16 23:06:28 -0500

    gh fix

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
+import React from 'react';
+import { HashRouter, Switch, Route } from "react-router-dom";
 import Home from "./Home";
 import Alumni from "./Alumni";
 
 export default function App() {
   return (
-    <Router>
-        <Switch>
-          <Route path="/alumni">
-            <Alumni />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-    </Router>
+    <HashRouter basename="/">
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/alumni" component={Alumni} />
+      </Switch>
+    </HashRouter>
   );
 }
